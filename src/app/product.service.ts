2ed848e47985df5568da244911fbbf4682306e8c
Prajwal shetty <M1053574@mindtree.com>
2020-04-25 15:09:35 +0530

    changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http, private response: Response) { }
+  private _albumurl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
 
   public getAlbum(id: number) {
-
+    return this._http.get(this._albumurl).pipe(map((response) => {
+      response.json();
+    }))
   }
 }
